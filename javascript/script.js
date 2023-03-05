b4121d8db0523c5b4650e5c4f2330619f08b522b
Suman Kumar <107740609+sumankr07@users.noreply.github.com>
2023-03-05 16:30:57 +0530

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,306 @@
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope) {
+    //-----------------Latest Update------------------//
+
+    //Update Section 1----------------------------------
+    $scope.update1 = "Section 1";
+    $scope.update1_link = "latestJob.html";
+
+    //Update Section 2----------------------------------
+    $scope.update2 = "Section 2";
+    $scope.update2_link = "latestJob.html";
+
+    //Update Section 3----------------------------------
+    $scope.update3 = "Section 3";
+    $scope.update3_link = "latestJob.html";
+
+    //Update Section 4----------------------------------
+    $scope.update4 = "Section 4";
+    $scope.update4_link = "latestJob.html";
+
+    //Update Section 5----------------------------------
+    $scope.update5 = "Section 5";
+    $scope.update5_link = "latestJob.html";
+
+    //Update Section 6----------------------------------
+    $scope.update6 = "Section 6";
+    $scope.update6_link = "latestJob.html";
+
+    //-----------------Latest Update End--------------//
+
+    //------------------------Result------------------//
+
+    //Result Section 1----------------------------------
+    $scope.result1 = "Section 1";
+    $scope.result1_link = "latestJob.html";
+
+    //Result Section 2----------------------------------
+    $scope.result2 = "Section 2";
+    $scope.result2_link = "latestJob.html";
+
+    //Result Section 3----------------------------------
+    $scope.result3 = "Section 3";
+    $scope.result3_link = "latestJob.html";
+
+    //Result Section 4----------------------------------
+    $scope.result4 = "Section 4";
+    $scope.result4_link = "latestJob.html";
+
+    //Result Section 5----------------------------------
+    $scope.result5 = "Section 5";
+    $scope.result5_link = "latestJob.html";
+
+    //Result Section 6----------------------------------
+    $scope.result6 = "Section 6";
+    $scope.result6_link = "latestJob.html";
+
+    //Result Section 7----------------------------------
+    $scope.result7 = "Section 7";
+    $scope.result7_link = "latestJob.html";
+
+    //Result Section 8----------------------------------
+    $scope.result8 = "Section 8";
+    $scope.result8_link = "latestJob.html";
+
+    //Result Section 9----------------------------------
+    $scope.result9 = "Section 9";
+    $scope.result9_link = "latestJob.html";
+
+    //Result Section 10----------------------------------
+    $scope.result10 = "Section 10";
+    $scope.result10_link = "latestJob.html";
+
+    //------------------Result End---------------------//
+
+    //---------------- ----Admit Card------------------//
+
+    //admit Section 1----------------------------------
+    $scope.admit1 = "Section 1";
+    $scope.admit1_link = "latestJob.html";
+
+    //admit Section 2----------------------------------
+    $scope.admit2 = "Section 2";
+    $scope.admit2_link = "latestJob.html";
+
+    //admit Section 3----------------------------------
+    $scope.admit3 = "Section 3";
+    $scope.admit3_link = "latestJob.html";
+
+    //admit Section 4----------------------------------
+    $scope.admit4 = "Section 4";
+    $scope.admit4_link = "latestJob.html";
+
+    //admit Section 5----------------------------------
+    $scope.admit5 = "Section 5";
+    $scope.admit5_link = "latestJob.html";
+
+    //admit Section 6----------------------------------
+    $scope.admit6 = "Section 6";
+    $scope.admit6_link = "latestJob.html";
+
+    //admit Section 7----------------------------------
+    $scope.admit7 = "Section 7";
+    $scope.admit7_link = "latestJob.html";
+
+    //admit Section 8----------------------------------
+    $scope.admit8 = "Section 8";
+    $scope.admit8_link = "latestJob.html";
+
+    //admit Section 9----------------------------------
+    $scope.admit9 = "Section 9";
+    $scope.admit9_link = "latestJob.html";
+
+    //admit Section 10----------------------------------
+    $scope.admit10 = "Section 10";
+    $scope.admit10_link = "latestJob.html";
+
+    //------------------Admit Card End---------------//
+
+    //-------------------Latest Job------------------//
+
+    //job Section 1----------------------------------
+    $scope.job1 = "Section 1";
+    $scope.job1_link = "latestJob.html";
+
+    //job Section 2----------------------------------
+    $scope.job2 = "Section 2";
+    $scope.job2_link = "latestJob.html";
+
+    //job Section 3----------------------------------
+    $scope.job3 = "Section 3";
+    $scope.job3_link = "latestJob.html";
+
+    //job Section 4----------------------------------
+    $scope.job4 = "Section 4";
+    $scope.job4_link = "latestJob.html";
+
+    //job Section 5----------------------------------
+    $scope.job5 = "Section 5";
+    $scope.job5_link = "latestJob.html";
+
+    //job Section 6----------------------------------
+    $scope.job6 = "Section 6";
+    $scope.job6_link = "latestJob.html";
+
+    //job Section 7----------------------------------
+    $scope.job7 = "Section 7";
+    $scope.job7_link = "latestJob.html";
+
+    //job Section 8----------------------------------
+    $scope.job8 = "Section 8";
+    $scope.job8_link = "latestJob.html";
+
+    //job Section 9----------------------------------
+    $scope.job9 = "Section 9";
+    $scope.job9_link = "latestJob.html";
+
+    //job Section 10----------------------------------
+    $scope.job10 = "Section 10";
+    $scope.job10_link = "latestJob.html";
+
+    //----------------Latest Job End-----------------//
+
+    //------------------Answer Key------------------//
+
+    //answer Section 1----------------------------------
+    $scope.answer1 = "Section 1";
+    $scope.answer1_link = "latestanswer.html";
+
+    //answer Section 2----------------------------------
+    $scope.answer2 = "Section 2";
+    $scope.answer2_link = "latestanswer.html";
+
+    //answer Section 3----------------------------------
+    $scope.answer3 = "Section 3";
+    $scope.answer3_link = "latestanswer.html";
+
+    //answer Section 4----------------------------------
+    $scope.answer4 = "Section 4";
+    $scope.answer4_link = "latestanswer.html";
+
+    //answer Section 5----------------------------------
+    $scope.answer5 = "Section 5";
+    $scope.answer5_link = "latestanswer.html";
+
+    //answer Section 6----------------------------------
+    $scope.answer6 = "Section 6";
+    $scope.answer6_link = "latestanswer.html";
+
+    //answer Section 7----------------------------------
+    $scope.answer7 = "Section 7";
+    $scope.answer7_link = "latestanswer.html";
+
+    //------------------Answer Key End-------------------//
+
+    //------------------------Syllabus------------------//
+
+    //syllabus Section 1----------------------------------
+    $scope.syllabus1 = "Section 1";
+    $scope.syllabus1_link = "latestsyllabus.html";
+
+    //syllabus Section 2----------------------------------
+    $scope.syllabus2 = "Section 2";
+    $scope.syllabus2_link = "latestsyllabus.html";
+
+    //syllabus Section 3----------------------------------
+    $scope.syllabus3 = "Section 3";
+    $scope.syllabus3_link = "latestsyllabus.html";
+
+    //syllabus Section 4----------------------------------
+    $scope.syllabus4 = "Section 4";
+    $scope.syllabus4_link = "latestsyllabus.html";
+
+    //syllabus Section 5----------------------------------
+    $scope.syllabus5 = "Section 5";
+    $scope.syllabus5_link = "latestsyllabus.html";
+
+    //syllabus Section 6----------------------------------
+    $scope.syllabus6 = "Section 6";
+    $scope.syllabus6_link = "latestsyllabus.html";
+
+    //syllabus Section 7----------------------------------
+    $scope.syllabus7 = "Section 7";
+    $scope.syllabus7_link = "latestsyllabus.html";
+
+    //------------------syllabus End---------------------//
+
+    //------------------------Admission------------------//
+
+    //adm Section 1----------------------------------
+    $scope.adm1 = "Section 1";
+    $scope.adm1_link = "latestadm.html";
+
+    //adm Section 2----------------------------------
+    $scope.adm2 = "Section 2";
+    $scope.adm2_link = "latestadm.html";
+
+    //adm Section 3----------------------------------
+    $scope.adm3 = "Section 3";
+    $scope.adm3_link = "latestadm.html";
+
+    //adm Section 4----------------------------------
+    $scope.adm4 = "Section 4";
+    $scope.adm4_link = "latestadm.html";
+
+    //adm Section 5----------------------------------
+    $scope.adm5 = "Section 5";
+    $scope.adm5_link = "latestadm.html";
+
+    //adm Section 6----------------------------------
+    $scope.adm6 = "Section 6";
+    $scope.adm6_link = "latestadm.html";
+
+    //adm Section 7----------------------------------
+    $scope.adm7 = "Section 7";
+    $scope.adm7_link = "latestadm.html";
+
+    //------------------Admission End---------------------//
+
+    //------------------------Up Coming Exam------------------//
+
+    //comingExam Section 1----------------------------------
+    $scope.comingExam1 = "Section 1";
+    $scope.comingExam1_link = "latestcomingExam.html";
+
+    //comingExam Section 2----------------------------------
+    $scope.comingExam2 = "Section 2";
+    $scope.comingExam2_link = "latestcomingExam.html";
+
+    //comingExam Section 3----------------------------------
+    $scope.comingExam3 = "Section 3";
+    $scope.comingExam3_link = "latestcomingExam.html";
+
+    //comingExam Section 4----------------------------------
+    $scope.comingExam4 = "Section 4";
+    $scope.comingExam4_link = "latestcomingExam.html";
+
+    //comingExam Section 5----------------------------------
+    $scope.comingExam5 = "Section 5";
+    $scope.comingExam5_link = "latestcomingExam.html";
+
+    //------------------Up Coming Exam End---------------------//
+
+    //------------------------Importants link Exam------------------//
+
+    //importantlink Section 1----------------------------------
+    $scope.importantlink1 = "Section 1";
+    $scope.importantlink1_link = "latestimportantlink.html";
+
+    //importantlink Section 2----------------------------------
+    $scope.importantlink2 = "Section 2";
+    $scope.importantlink2_link = "latestimportantlink.html";
+
+    //importantlink Section 3----------------------------------
+    $scope.importantlink3 = "Section 3";
+    $scope.importantlink3_link = "latestimportantlink.html";
+
+    //importantlink Section 4----------------------------------
+    $scope.importantlink4 = "Section 4";
+    $scope.importantlink4_link = "latestimportantlink.html";
+
+    //importantlink Section 5----------------------------------
+    $scope.importantlink5 = "Section 5";
+    $scope.importantlink5_link = "latestimportantlink.html";
+
+    //------------------Importants link End---------------------//
+});
